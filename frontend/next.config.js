7dca04b2295190fbe20a18e191ce3571d8e3a44a
Divya Reddy Suram <142125052+suramdivyareddy@users.noreply.github.com>
2025-01-31 22:20:05 -0500

    Add files via upload

---
+++ frontend/next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ]
+  },
+  images: {
+    domains: ['65.0.62.180'], // Add your backend domain here if you're serving images
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+}
+
+module.exports = nextConfig
